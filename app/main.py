1875b97e81004ad130158bc3adbc67798da9c142
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-03-11 19:08:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,30 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
 from app.config import load_config
-from app.handlers import user
+from app.db import init_db
+from app.handlers.user import router as user_router
+from app.handlers.admin import router as admin_router
 
 
-async def start():
+async def start_bot():
     config = load_config()
+    init_db()
 
     bot = Bot(
         token=config.bot_token,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
 
     dp = Dispatcher()
-
-    dp.include_router(user.router)
+    dp.include_router(user_router)
+    dp.include_router(admin_router)
 
     print("Bot ishga tushdi...")
-
     await dp.start_polling(bot)
 
 
 def main():
-    asyncio.run(start())
+    asyncio.run(start_bot())
