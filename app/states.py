c9794a8a8af6a782cc6d352a9dd2269c8d06f4f4
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-03-11 19:06:13 +0300

    Update states.py

--- app/states.py
+++ app/states.py
@@ -1,11 +1,12 @@
-from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.state import State, StatesGroup
 
 
-class OrderState(StatesGroup):
-    user_id_code = State()
-    full_name = State()
-    product = State()
-    phone = State()
-    country = State()
-    region = State()
-    address = State()
+class OrderStates(StatesGroup):
+    waiting_for_id = State()
+    waiting_for_full_name = State()
+    waiting_for_product = State()
+    waiting_for_phone = State()
+    waiting_for_country = State()
+    waiting_for_region = State()
+    waiting_for_address = State()
+    waiting_for_review = State()
